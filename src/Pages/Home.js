91828f05d19219d8306f058721412e725006b867
Mace <maceplus@gmail.com>
2020-12-09 14:56:32 +0000

    add className

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,13 +6,13 @@ export default class Home extends Component {
   render() {
     return (
       <div className="main-content">
+        <div className="header">
           <h1>CatchMe</h1>
           <p>The app that keeps you running</p>
-          <nav>
+          </div>
         <button><a href="https://www.bbc.co.uk">How to play</a></button>
         <button><Link to={"/race"}>Create a race</Link></button>
-        <button><a href="https://www.bbc.co.uk">Leaderboard</a></button>
-        </nav>
+        <button>Leaderboard</button>
         <img src={logo} alt="Logo" />
         </div>
     )
