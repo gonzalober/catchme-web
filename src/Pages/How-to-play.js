cefd5fc5db0a2b6c7b9af3deb6bf4cc957f83026
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-12-09 16:38:09 +0000

    Added lobby and how-to-play pages

---
+++ src/Pages/How-to-play.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class HowToPlay extends Component {
+  render() {
+    return (
+      <div className="main-content">
+        <div className="race-end">
+          <h1>How to Play</h1>
+            <p>1 - Create race</p>
+            <p>2 - Add users</p>
+            <p>3 - Start race</p>
+            <p>4 - Race ends</p>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/Pages/Lobby.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Lobby extends Component {
+  render() {
+    return (
+      <div className="main-content">
+        <div className="race-end">
+          <h1>Lobby</h1>
+            <p>Lobby Code: AHFB624</p>
+            <p>Participants: User 1</p>
+            <p>Start Race</p>
+            <p>Edit race settings</p>
+        </div>
+      </div>
+    )
+  }
+}
