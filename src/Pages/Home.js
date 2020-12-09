66b0d55627749c7e99bf984bc3c844d122f86761
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-12-09 13:09:33 +0000

    Fixed error on render race route not found

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import logo from '../images/logo.gif'
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Setup from './Race-setup.js'
+import App from '../App'
 
 export default class Home extends Component {
   render() {
     return (
       <div className="main-content">
-        <div className="header">
           <h1>CatchMe</h1>
           <p>The app that keeps you running</p>
-        </div>
+          <nav>
         <button><a href="https://www.bbc.co.uk">How to play</a></button>
-        <button><a href="https://www.bbc.co.uk">Create a race</a></button>
+        <button><Link to="/race">Create a race</Link></button>
         <button><a href="https://www.bbc.co.uk">Leaderboard</a></button>
+        </nav>
         <img src={logo} alt="Logo" />
-      </div>
+        </div>
     )
   }
 }
