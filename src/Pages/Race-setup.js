dd3ef32e09aa2af77a567161b95e6c8822a57861
Mace <maceplus@gmail.com>
2020-12-09 14:55:32 +0000

    remove react-buttons

--- src/Pages/Race-setup.js
+++ src/Pages/Race-setup.js
@@ -6,20 +6,10 @@ export default class Setup extends Component {
     return (
       <div className="main-content">
         <div className="lobby-creation">
-          <h1>Lobby Creation</h1>
+          <h1>CatchMe</h1>
           <p>Race parameters</p>
         </div>
-        <RadioGroup onChange={ this.onChange } horizontal>
-          <RadioButton value="1km">
-            1km
-          </RadioButton>
-          <RadioButton value="2km">
-            2km
-          </RadioButton>
-          <RadioButton value="1km">
-            3km
-          </RadioButton>
-        </RadioGroup>
+
       </div>
     )
   }
