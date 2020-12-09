b58bea9123162fa9f378b4bfe15357cce09a0e80
Mace <maceplus@gmail.com>
2020-12-09 12:18:46 +0000

    add links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import Home from './Pages/Home.js'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Setup from './Pages/Race-setup.js'
+import Home from './Pages/Home.js';
 
 function App() {
   return (
+    <Router>
     <div className="container">
-      <Home />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/race">
+          <Setup />
+        </Route>
+      </Switch>
     </div>
+    </Router>
+
   );
 }
 
