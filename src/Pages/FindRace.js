a3cbc6b7846f244892f9570643d417f35a6664af
Mace <maceplus@gmail.com>
2020-12-15 00:46:25 +0000

    centering items

--- src/Pages/FindRace.js
+++ src/Pages/FindRace.js
@@ -9,9 +9,10 @@ export default class FindRace extends Component {
       <div className="main-content">
         <Header/>
           <div className="race-finder">
-            <p>Race Finder</p>
+            <h2>Race Finder</h2>
+            <FindRaceForm />
           </div>
-        <FindRaceForm />
+        
       </div>
     )
   }
