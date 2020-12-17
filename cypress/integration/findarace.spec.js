381c8dcf39801c0c406f99d45c410dcf55bdb7ee
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-12-17 15:11:23 +0000

    Added tests for finding a race

--- cypress/integration/findarace.spec.js
+++ cypress/integration/findarace.spec.js
@@ -1,10 +1,10 @@
-describe('Testing Homepage', function() {
+describe('Testing Find Race Page', function() {
 
 
-  describe('Homepage Links', function() {
-    it('click find a race button', function() {
+  describe('Find Race Page', function() {
+    it('check content is correct', function() {
       cy.visit('/findrace')
-      cy.get('button').contains('Find a race').click()
+      cy.get('input').contains('Find Race').click()
       cy.location('pathname').should('include', 'findrace')
       cy.readFile('src/components/SoundEffects/Button-sound.mp3', 'base64').then((mp3) => {
         const uri = 'data:audio/mp3;base64,' + mp3
@@ -18,6 +18,36 @@ describe('Testing Homepage', function() {
       cy.get('input').contains('Find Race')
       cy.get('[alt="home-icon"]').should('be.visible')
       cy.get('[alt="plus-icon"]').should('be.visible')
+      cy.get('p').contains('KiMaGoDa')
+    })
+  })
+
+  describe('Find Race Page', function() {
+    it('check content is correct', function() {
+      cy.visit('/findrace')
+      cy.get('input').contains('Find Race')
+      cy.location('pathname').should('include', 'findrace')
+      cy.readFile('src/components/SoundEffects/Button-sound.mp3', 'base64').then((mp3) => {
+        const uri = 'data:audio/mp3;base64,' + mp3
+        const audio = new Audio(uri)
+
+        audio.play()
+      })
+      cy.get('h1').contains('CatchMe')
+      cy.get('h2').contains('Race Finder')
+      cy.get('p').contains('Enter your name:')
+      cy.get('input').contains('Find Race')
+      cy.get('[alt="home-icon"]').should('be.visible')
+      cy.get('[alt="plus-icon"]').should('be.visible')
+      cy.get('p').contains('KiMaGoDa')
+      cy.get('input[type="text"]').first().type('230')
+      cy.get('input').contains('Find Race').click()
+      cy.get('h1').contains('CatchMe')
+      cy.get('[alt="home-icon"]').should('be.visible')
+      cy.get('p').contains('Code: 230')
+      cy.get('p').contains('Distance: 50m')
+      cy.get('li').contains('Jimmy')
+      cy.get('button').contains('Ready!')
       cy.get('p').contains('KiMaGoDa')
     })
   })
