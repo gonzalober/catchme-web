b4d5d0422abccea46c43a567b01df7a51cd54a1e
Kiril <kiril@192.168.1.105>
2020-12-17 10:16:24 +0000

    gg

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -41,22 +41,6 @@ export default class Home extends Component {
           <h1>CatchMe</h1>
           <p>The app that keeps you running</p>
         </div>
-        <div className="home-buttons">
-          <button>
-            <Link to={"/how-to-play"}>How to play</Link>
-          </button>
-          <button>
-            <Link to={"/createrace"}>Create a race</Link>
-          </button>
-          <button>
-            <Link to={"/findrace"}>Find a race</Link>
-          </button>
-          <button>
-            <Link to={"/leaderboard"}>Leaderboard</Link>
-          </button>
-          <img className="running-boy" src={logo} alt="Logo" />
-        </div>
-        <Footer />
       <div className="home-buttons">
         <button onClick={() =>  Bruh.play()}><Link to={"/how-to-play"}>How to play</Link></button>
         <button onClick={() =>  Click.play()}><Link to={"/createrace"}>Create a race</Link></button>
