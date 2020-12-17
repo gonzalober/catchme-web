1d93cce5a9e7d2ee923f5fcae2348ee2d8cceac2
Mace <maceplus@gmail.com>
2020-12-17 00:46:31 +0000

    add button sounds

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,12 +4,30 @@ import {Link} from 'react-router-dom';
 import Footer from '../components/Footer';
 import UIfx from 'uifx';
 import GameOver from '../components/SoundEffects/gameover.mp3'
+import BRUH from '../components/SoundEffects/Bruh.MP3'
+import CLI from '../components/SoundEffects/Button-sound.mp3'
 
 export default class Home extends Component {
   render() {
 
     const beep = new UIfx(
       GameOver,
+      {
+        volume: 0.8,
+        throttleMs: 100
+      }
+    )
+
+    const Bruh = new UIfx(
+      BRUH,
+      {
+        volume: 0.8,
+        throttleMs: 100
+      }
+    )
+
+    const Click = new UIfx(
+      CLI,
       {
         volume: 0.8,
         throttleMs: 100
@@ -23,10 +41,10 @@ export default class Home extends Component {
         <p>The app that keeps you running</p>
       </div>
       <div className="home-buttons">
-        <button onClick={() => beep.play()}><Link to={"/how-to-play"}>How to play</Link></button>
-        <button><Link to={"/createrace"}>Create a race</Link></button>
-        <button><Link to={"/findrace"}>Find a race</Link></button>
-        <button><Link to={"/leaderboard"}>Leaderboard</Link></button>
+        <button onClick={() =>  Bruh.play()}><Link to={"/how-to-play"}>How to play</Link></button>
+        <button onClick={() =>  Click.play()}><Link to={"/createrace"}>Create a race</Link></button>
+        <button onClick={() =>  Click.play()}><Link to={"/findrace"}>Find a race</Link></button>
+        <button onClick={() =>  Click.play()}><Link to={"/leaderboard"}>Leaderboard</Link></button>
         <img className="running-boy" src={logo} alt="Logo" />
       </div>
       <Footer />
--- src/components/Sounds.js
+++ src/components/Sounds.js
@@ -2,15 +2,14 @@ import React, { Component } from "react";
 import GameOver from '../components/SoundEffects/gameover.mp3'
 import UIfx from "uifx";
 
-export default class Sound extends Component {
-  render() {
-    const beep = new UIfx(GameOver, {
-      volume: 0.8,
-      throttleMs: 100,
-    });
-    return(
-      <></>
+
+
+export default function Game() {
+  const beep = new UIfx(
+      GameOver,
+      {
+        volume: 0.8,
+        throttleMs: 100
+      }
     )
-  }
- 
 }
