ea21b7e6d92b509d0b596fab7c3f9cc5ce4fee45
Gonzalo Berhouet <gonzaloberhouet@MA333.fritz.box>
2020-12-17 09:52:54 +0000

    endRace without starttime

--- src/Pages/Race.js
+++ src/Pages/Race.js
@@ -1,19 +1,20 @@
 import React, { Component, useState, useEffect } from "react";
 import DistanceCalculator from "../components/DistanceCalculator2";
-import Header2 from '../components/Header2';
+import Header2 from "../components/Header2";
 import Map from "../components/Map";
-import  Footer from '../components/Footer';
-import StopwatchAutoStart from '../components/StopwatchAutoStart';
+import Footer from "../components/Footer";
+import StopwatchAutoStart from "../components/StopwatchAutoStart";
 
 function Race() {
   return (
     <div className="main-content">
       <DistanceCalculator />
-      <Header2/>
-        <div className="race-details">
-          <StopwatchAutoStart />
-          <Map />
-        </div>
+      <Header2 />
+      <div className="race-details">
+        <Map />
+        <br></br>
+        <StopwatchAutoStart />
+      </div>
       <Footer />
     </div>
   );
--- src/components/RaceEnd.js
+++ src/components/RaceEnd.js
@@ -27,36 +27,12 @@ export default function RaceEnd() {
     return minutes + ":" + seconds + ":" + milliseconds;
   }
 
-  function convertTimestamp(timestamp) {
-    var d = new Date(timestamp * 1000),
-      yyyy = d.getFullYear(),
-      mm = ("0" + (d.getMonth() + 1)).slice(-2),
-      dd = ("0" + d.getDate()).slice(-2),
-      hh = d.getHours(),
-      h = hh,
-      min = ("0" + d.getMinutes()).slice(-2),
-      ampm = "AM",
-      time;
-    if (hh > 12) {
-      h = hh - 12;
-      ampm = "PM";
-    } else if (hh === 12) {
-      h = 12;
-      ampm = "PM";
-    } else if (hh == 0) {
-      h = 12;
-    }
-    time = yyyy + "-" + mm + "-" + dd + ", " + h + ":" + min + " " + ampm;
-    return time;
-  }
-
   return (
     <div className="main-content">
       <div className="lobby-page">
         <h1>Race Complete!</h1>
         <p>Race code: {race?.id}</p>
         <p>Distance completed: {race && race.distance}m</p>
-        <p>Start Time: {race && convertTimestamp(race.startTime)}</p>
         <p>Time of the Race: {race && msToTime(maxScore())}</p>
         <div>
           Score:{" "}
